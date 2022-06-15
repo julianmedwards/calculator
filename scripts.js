26154d5e5b99521303773b974ab99396b7e68d51
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-15 13:07:47 -0500

    Working calculator script (1st draft/untested).

--- scripts.js
+++ scripts.js
@@ -1,48 +1,160 @@
-let buttons = document
-    .getElementById('calculator')
-    .getElementsByTagName('button')
-for (button of buttons) {
-    button.addEventListener('click', buttonPress)
-}
-
 const calculator = {
     displayVal: document.getElementsByName('display'),
-    vals: {},
-    result: undefined,
-    buttonPress: buttonPress,
-    enterDigit: enterDigit,
-    setOperator: setOperator,
-    clear: clear,
-    calculate: calculate,
+    resetState: true,
+    lastInputNum: true,
+    decimelPlaced: false,
+}
+// Calculator Functions
+calculator.buttonPress = function buttonPress() {
+    let buttonFunction = calculator.identifyButton(this)
+
+    let updateData = buttonFunction(this)
+
+    calculator.updateDisplay(updateData)
+
+    calculator.updateStates(updateData)
 }
 
-function buttonPress() {
-    console.log('Button pressed.')
-
-    let handleButton = identifyButton(this)
-
-    handleButton()
-}
-
-function identifyButton(buttonEl) {
+calculator.identifyButton = function identifyButton(buttonEl) {
     if (buttonEl.getAttribute('data-type') == 'digit') {
-        return enterDigit
+        return calculator.addDigit
+    } else if (buttonEl.getAttribute('data-type') == 'decimel') {
+        return calculator.placeDecimel
     } else if (buttonEl.getAttribute('data-type') == 'operator') {
-        return setOperator
+        return calculator.addOperator
     } else if (buttonEl.getAttribute('data-type') == 'clear') {
-        return clear
+        return calculator.clear
     } else if (buttonEl.getAttribute('data-type') == 'submit') {
-        return calculate
+        return calculator.calculate
     } else {
         console.error('Unrecognized button!')
     }
 }
 
-function enterDigit() {}
-function setOperator() {}
-function clear() {
-    console.log('Clearing')
-    calculator.vals = {}
-    calculator.displayVal[0].textContent = '0'
+calculator.addDigit = function addDigit(button) {
+    let digit = button.textContent.trim()
+
+    if (calculator.resetState) {
+        return {val: digit, updateType: 'replaceLast', num: true}
+    } else {
+        return {val: digit, updateType: 'add', num: true}
+    }
 }
-function calculate() {}
+
+calculator.placeDecimel = function placeDecimel() {
+    if (calculator.resetState) {
+        return {val: '.', updateType: 'replaceLast', num: true}
+    } else {
+        if (calculator.decimelPlaced) {
+            return {
+                val: undefined,
+                updateType: 'pass',
+                num: calculator.lastInputNum,
+            }
+        } else {
+            return {val: '.', updateType: 'add', num: true, decimel: true}
+        }
+    }
+}
+
+calculator.addOperator = function addOperator(button) {
+    let operator = button.textContent.trim()
+    if (calculator.lastInputNum == false) {
+        return {
+            val: ' ' + operator + ' ',
+            updateType: 'swapOp',
+            num: false,
+            decimel: false,
+        }
+    } else {
+        return {
+            val: ' ' + operator + ' ',
+            updateType: 'add',
+            num: false,
+            decimel: false,
+        }
+    }
+}
+
+calculator.clear = function clear() {
+    return {val: '0', updateType: 'replaceAll', num: true, decimel: false}
+}
+
+calculator.calculate = function calculate() {
+    let equation = calculator.displayVal[0].textContent.split(' ')
+
+    for (let opIndex = 1; equation[opIndex] != undefined; ) {
+        let operator = equation[opIndex]
+        let leftNum = equation[opIndex - 1]
+        let rightNum = equation[opIndex + 1]
+        if (operator == '*') {
+            equation.splice(opIndex - 1, 3, Number(leftNum) * Number(rightNum))
+        } else if (operator == '/') {
+            equation.splice(opIndex - 1, 3, Number(leftNum) / Number(rightNum))
+        } else {
+            opIndex += 2
+        }
+    }
+    for (let opIndex = 1; equation.length > 1; ) {
+        operator = equation[opIndex]
+        let leftNum = equation[opIndex - 1]
+        let rightNum = equation[opIndex + 1]
+        if (operator == '+') {
+            equation.splice(opIndex - 1, 3, Number(leftNum) + Number(rightNum))
+        } else {
+            equation.splice(opIndex - 1, 3, Number(leftNum) - Number(rightNum))
+        }
+    }
+
+    calculator.result = equation.toString()
+    decimelPresent = calculator.result.includes('.')
+
+    return {
+        val: calculator.result,
+        updateType: 'replaceAll',
+        num: true,
+        decimel: decimelPresent,
+    }
+}
+
+calculator.updateDisplay = function updateDisplay(data) {
+    if (data.updateType == 'add') {
+        calculator.displayVal[0].textContent += data.val
+    } else if (data.updateType == 'swapOp') {
+        calculator.displayVal[0].textContent =
+            calculator.displayVal[0].textContent.slice(0, -3)
+        calculator.displayVal[0].textContent += data.val
+    } else if (data.updateType == 'replaceLast') {
+        let vals = calculator.displayVal[0].textContent.split(' ')
+        vals[vals.length - 1] = data.val
+        calculator.displayVal[0].textContent = vals.join(' ')
+    } else if (data.updateType == 'replaceAll') {
+        calculator.displayVal[0].textContent = data.val
+    } else if (data.updateType == 'pass') {
+    } else {
+        console.error('Unknown display update type!')
+    }
+}
+
+// data keys: val (str), updateType (str), num (bool), decimel (bool)
+calculator.updateStates = function updateStates(data) {
+    calculator.lastInputNum = data.num
+
+    if (data.val == '0' || data.updateType == 'replaceAll') {
+        calculator.resetState = true
+    } else {
+        calculator.resetState = false
+    }
+
+    if (data.decimel != undefined) {
+        calculator.decimelPlaced = data.decimel
+    }
+}
+
+// Event Listeners
+let buttons = document
+    .getElementById('calculator')
+    .getElementsByTagName('button')
+for (button of buttons) {
+    button.addEventListener('click', calculator.buttonPress)
+}
